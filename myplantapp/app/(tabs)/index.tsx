2b6ce3d4d28caf8fdc1084e3785f3c0e6233c273
Julia Mikolajczyk <jmikolajczyk2@nait.ca>
2025-02-27 20:26:12 -0700

    Building a screen

--- myplantapp/app/(tabs)/index.tsx
+++ myplantapp/app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Buttons";
+
+const PlaceHolderImage = require("../../assets/images/background-image.png");
 
 // export default makes it the entry point of the navigation route (always goes to index page)
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage}></ImageViewer>
+      </View>
+      <View style = {styles.footerContainer}>
+        <Button label = "Choose a photo" theme="primary"></Button>
+        <Button label = "Use this photo"></Button>
+      </View>
+      
+      {/* <Text style={styles.text}>Hello World</Text> */}
       {/* <Link href={"/about"} style = {styles.button}> Go to About Screen </Link> */}
     </View>
   );
@@ -15,15 +27,13 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#25292e",
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "white",
+  imageContainer: {
+    flex: 1
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff"
+  footerContainer:{
+    flex: 1/3,
+    alignItems: "center",
   }
 });
